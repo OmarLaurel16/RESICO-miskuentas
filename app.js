ebcb43f9ad09f8a5cc14aacf3f679aac0a950299
OmarLaurel16 <oh383251@gmail.com>
2026-06-18 15:51:09 +0000

    Jun 18, 2026, 9:51 AM

--- app.js
+++ app.js
@@ -1260,7 +1260,7 @@ var _carousel = {
       titulo: "Gastos en efectivo mayores a $2,000",
       texto:
         "Todo gasto mayor a $2,000 pagado en efectivo es considerado no deducible por la autoridad fiscal.",
-      icon: '<rect width="20" height="14" x="2" y="5" rx="2"/><path d="M12 18V6"/>',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-dollar-sign-icon lucide-circle-dollar-sign"><circle cx="12" cy="12" r="10"/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/><path d="M12 18V6"/></svg>',
     },
     {
       titulo: "Compra de vehículos",
