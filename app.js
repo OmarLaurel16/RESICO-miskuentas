3948ad63939ac7ce64f028a7ea1dff1ed461edf4
OmarLaurel16 <oh383251@gmail.com>
2026-07-03 21:48:48 +0000

    Jul 3, 2026, 3:48 PM

--- app.js
+++ app.js
@@ -1608,7 +1608,7 @@ const canvas = document.getElementById("animacion");
 const ctx = canvas.getContext("2d");
 
 const TOTAL_FRAMES = 44;
-const FPS = 8;
+const FPS = 11;
 const SCALE = 0.35;
 
 const frames = [];
