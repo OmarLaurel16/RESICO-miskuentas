3e7d12e930a2b1426dc9e9e12c9b0a5fab15c79a
OmarLaurel16 <oh383251@gmail.com>
2026-07-17 16:52:51 +0000

    Jul 17, 2026, 10:52 AM

--- app.js
+++ app.js
@@ -1424,7 +1424,7 @@ var _carousel = {
       texto:
         "Cuando cobres en parcialidades, emite el complemento de pago dentro de los primeros 5 días del mes siguiente. Evitarás diferencias con el SAT.",
       icon: '<rect width="18" height="18" x="3" y="4" rx="2" ry="2"/><line x1="16" x2="16" y1="2" y2="6"/><line x1="8" x2="8" y1="2" y2="6"/><line x1="3" x2="21" y1="10" y2="10"/>',
-      imagen: "ComplementoPago.jpg",
+      imagen: "Complementos.png",
     },
     {
       titulo: "Revisa tu buzón tributario",
