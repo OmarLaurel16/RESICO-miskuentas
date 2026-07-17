416337540ddb0f4c118cbc257930741800ae70ca
OmarLaurel16 <oh383251@gmail.com>
2026-07-17 16:49:19 +0000

    Jul 17, 2026, 10:49 AM

--- app.js
+++ app.js
@@ -1410,7 +1410,7 @@ var _carousel = {
       texto:
         "Procura que la forma de pago sea distinta a efectivo para que sea deducible.",
       icon: '<path d="M14 13h2a2 2 0 0 1 2 2v2a2 2 0 0 0 4 0v-6.998a2 2 0 0 0-.59-1.42L18 5"/><path d="M14 21V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v16"/><path d="M2 21h13"/><path d="M3 9h11"/>',
-      imagen: "Gasolina.jpg",
+      imagen: "Gasolina.png",
     },
     {
       titulo: "Gastos en efectivo mayores a $2,000",
